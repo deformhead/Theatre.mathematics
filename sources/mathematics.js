2b5591673dffab78fbba3b242b0708fa6bdc41e4
Alexandre Blondeau <deformhead@gmail.com>
2018-04-14 23:01:48 +0200

    add 'hypothenuse' function

--- sources/mathematics.js
+++ sources/mathematics.js
@@ -1,3 +1,9 @@
+export function hypothenuse(ab, ac) {
+
+    // return the hypothenuse value of a right-angled triangle given its two smallest sides
+    return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
+};
+
 export function sigmoid(x) {
 
     // return the sigmoid value of given x
