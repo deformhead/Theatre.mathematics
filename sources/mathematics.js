c0cda78cda323f3f380ec02ffc12b4571ca217d3
Alexandre Blondeau <deformhead@gmail.com>
2018-05-21 12:07:48 +0200

    update export type

--- sources/mathematics.js
+++ sources/mathematics.js
@@ -1,23 +1,31 @@
-export function hypothenuse(ab, ac) {
+function hypothenuse(ab, ac) {
 
     // return the hypothenuse value of a right-angled triangle given its two smallest sides
     return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
-};
+}
 
-export function identity(x) {
+function identity(x) {
 
     // return the identity of given x
     return x;
-};
+}
 
-export function normalize(x, minimum, maximum) {
+function normalize(x, minimum, maximum) {
 
     // return the normalized value with (0, 1) range of given x with given (minimum, maximum) range
     return (x - minimum) / (maximum - minimum);
-};
+}
 
-export function sigmoid(x) {
+function sigmoid(x) {
 
     // return the sigmoid value of given x
     return 1 / (1 + Math.exp(-x));
+}
+
+export {
+
+    hypothenuse,
+    identity,
+    normalize,
+    sigmoid
 };
